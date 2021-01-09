c5f61bc407f9e8001b680381b858cb184c415798
ZMomo <mh.zeghdoudi@gmail.com>
2021-01-09 17:36:01 +0100

    🚧 :hammer: adding TextInput atom component

---
+++ src/components/atoms/TextInput/index.js
@@ -0,0 +1,16 @@
+import { useContext } from "react"
+
+import { FormInputContext } from '../../molecules/FormInput'
+
+export const TextInput = () => {
+    const { text, setText } = useContext(FormInputContext)
+    
+    const handler = (e) => {
+        e.preventDefault()
+        setText(e.target.value)
+    }
+
+    return (
+        <input type="text" onInput={handler} value={text}></input>
+    )
+}
