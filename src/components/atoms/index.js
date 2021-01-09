ad94e63e1b0f3a30af7c939ef38ab535ea6b5861
ZMomo <mh.zeghdoudi@gmail.com>
2021-01-09 17:36:46 +0100

    🚧 :hammer: adding index file for atoms components

---
+++ src/components/atoms/index.js
@@ -0,0 +1,7 @@
+import { Button } from './Button'
+import { Card } from './Card'
+import { Label } from './Label'
+import { Range } from './Range'
+import { TextInput } from './TextInput'
+
+export { Button, Card, Label, Range, TextInput }
