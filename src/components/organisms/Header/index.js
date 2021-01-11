816e63d93e7715372fb8916572dc198f955fd6df
ZMomo <mh.zeghdoudi@gmail.com>
2021-01-11 02:16:44 +0100

    🚧 adding redirect to homepage when Demodrop logo (in Header componenet) is clicked

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -14,7 +14,7 @@ export const Header = () => {
 
     return (
         <header>
-            <h1>DEMODROP</h1>
+            <h1 role="button" onClick={() => history.push('/')}>DEMODROP</h1>
             <SearchInput></SearchInput>
             <Button text="Upload" onClick={() => history.push('/upload')}></Button>
             <Button text="Sign Out" onClick={handleSignOut}></Button>
