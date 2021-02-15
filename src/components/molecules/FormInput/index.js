c57cb849d8b301516c43e1454128e792e5a8fc5e
ZMomo <mh.zeghdoudi@gmail.com>
2021-02-15 18:31:07 +0100

    ♻️ 🔥Refactoring, simplifying Forms, following better atomic design principles

--- src/components/molecules/FormInput/index.js
+++ src/components/molecules/FormInput/index.js
@@ -1,24 +1,8 @@
-import { createContext, useState } from 'react'
-import { Label, TextInput } from '../../atoms/'
+import { Label } from '../../atoms/'
 
-
-export const FormInputContext = createContext()
-
-const FormInputContextProvider = ({ children, value }) => {
-    const [text, setText] = useState(value || '')
-    
-    return (
-        <FormInputContext.Provider value={{ text, setText }}>{children}</FormInputContext.Provider>
-    )
-}
-
-export const FormInput = ({ value, label }) => {
-    return (
-        <span>
-            <FormInputContextProvider value={value}>
-                <Label>{label}</Label>
-                <TextInput></TextInput>
-            </FormInputContextProvider>
-        </span>
-    )
-}
+export const FormInput = ({ label, children }) => (
+    <span>
+        <Label>{label}</Label>
+        {children}
+    </span>
+)
