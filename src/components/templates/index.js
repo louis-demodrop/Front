cc786822cbd9ce183e799ed56c6097a855363edc
louis-demodrop <louisdemodrop@Lon.localdomain>
2021-03-13 18:42:40 +0100

    :bug: :lipstick: forms in center

--- src/components/templates/index.js
+++ src/components/templates/index.js
@@ -2,5 +2,6 @@ import { FeedTemplate } from "./FeedTemplate"
 import { SignInTemplate } from "./SignInTemplate"
 import { SignUpTemplate } from "./SignUpTemplate"
 import { UploadTemplate } from "./UploadTemplate"
+import './index.css'
 
 export { FeedTemplate, SignInTemplate, SignUpTemplate, UploadTemplate }
