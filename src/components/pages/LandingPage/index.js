7c16ad6f8512153a068340b93491b8db71e9e36e
ZMomo <mh.zeghdoudi@gmail.com>
2021-03-17 16:02:36 +0100

    ✨🚧  new page: Landing Page, TODO: css

---
+++ src/components/pages/LandingPage/index.js
@@ -0,0 +1,9 @@
+import { Button } from '../../atoms/';
+
+export const LandingPage = () => (
+    <>
+        <h1>DEMODROP</h1>
+        <Button>ENTER THE MATRIX</Button>
+        <span>A little bit lost? <a href="/signin">Follow the ...</a></span>   
+    </>
+);
